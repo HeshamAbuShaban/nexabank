0ca84225c72ddac4a9bddb7d057f1765ab2ac585
HP <heshamabushaban.developer@gmail.com>
2024-12-30 07:32:59 +0200

    feat: Implement authentication flow and design
    This commit introduces the following changes:
    
    - Added the authentication navigation graph for managing login and registration flows.
    - Integrated Material Design 3 (MD3) styles, colors, and themes for a modern and consistent look.
    - Designed the login and registration screens with UI elements and layouts.
    - Added a custom font to enhance the visual appeal of the application.

--- app/src/main/java/com/nexabank/core/NexaBankApp.kt
+++ app/src/main/java/com/nexabank/core/NexaBankApp.kt
@@ -1,7 +1,13 @@
 package com.nexabank.core
 
 import android.app.Application
+import com.google.android.material.color.DynamicColors
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class NexaBankApp : Application()
+class NexaBankApp : Application(){
+    override fun onCreate() {
+        super.onCreate()
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
+}
