e8e76e67d60dffb7c916fce921e0276c0a15e1c2
HP <heshamabushaban.developer@gmail.com>
2025-01-02 02:09:33 +0200

    better colors.xml, font, first attempt of make screens

--- app/src/main/java/com/nexabank/ui/fragments/LoginFragment.kt
+++ app/src/main/java/com/nexabank/ui/fragments/LoginFragment.kt
@@ -1,13 +1,15 @@
 package com.nexabank.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.nexabank.databinding.FragmentLoginBinding
+import com.nexabank.ui.MainActivity
 
-class LoginFragment:Fragment() {
+class LoginFragment : Fragment() {
     private lateinit var binding: FragmentLoginBinding
 
     override fun onCreateView(
@@ -25,6 +27,15 @@ class LoginFragment:Fragment() {
     }
 
     private fun init() {
+        setupListeners()
+    }
 
+    private fun setupListeners() {
+        binding.loginButton.setOnClickListener {
+            // we need to give context for the intent
+            val context = requireContext()
+            startActivity(Intent(context, MainActivity::class.java))
+            activity?.finish()
+        }
     }
 }
